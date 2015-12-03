b998c03c7bb1e21f63737ae468cac8b68bcab9db
NY-Z <q@qs-MacBook-Pro.local>
2015-12-03 21:03:24 +0800

    Submit
    Submit

---
+++ MaShangTong-Personal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MaShangTong-Personal
+//
+//  Created by jeaner on 15/11/10.
+//  Copyright © 2015年 jeaner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

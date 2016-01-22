559913195b58ea714dfaeeb5663fce9f4a3694c0
NY-Z <NY@appledeMacBook-Pro-10.local>
2016-01-22 10:33:24 +0800

    0122
    0122

---
+++ MaShangTong-Personal/Home/Home-personalInfo/View/MyNewsTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  MyNewsTableViewCell.m
+//  MaShangTong
+//
+//  Created by q on 15/12/10.
+//  Copyright © 2015年 NY. All rights reserved.
+//
+
+#import "MyNewsTableViewCell.h"
+
+@implementation MyNewsTableViewCell
+
+- (void)awakeFromNib {
+    
+}
+
+
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
